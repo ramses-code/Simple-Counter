0a0e986b53e39a99deed1be6fa6697e56d1055cb
Ramses Cevallos <cevallosramses@gmail.com>
2022-06-21 20:43:06 +0000

    created simple counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,36 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="main-container">
+			<div className="counter">
+				<p className="icon"><svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+					<path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+					<path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
+					</svg>
+				</p>
+				<p>
+					{props.sixthDigit}
+				</p>
+				<p>
+					{props.fifthDigit}
+				</p>
+				<p>
+					{props.fourthDigit}
+				</p>
+				<p>
+					{props.thirdDigit}
+				</p>
+				<p>
+					{props.secondDigit}
+				</p>
+				<p>
+					{props.firstDigit}
+				</p>
+			</div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,26 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+
+function secsCounter() {
+
+    let first = Math.floor(counter) % 10;
+    let second = Math.floor(counter / 10) % 10;
+    let third = Math.floor(counter / 100) % 10;
+    let fourth = Math.floor(counter / 1000) % 10;
+    let fifth = Math.floor(counter / 10000) % 10;
+    let sixth = Math.floor(counter / 100000) % 10;
+
+    ReactDOM.render(<Home 
+        firstDigit={first} 
+        secondDigit={second}
+        thirdDigit={third}
+        fourthDigit={fourth}
+        fifthDigit={fifth}
+        sixthDigit={sixth}
+        />, document.querySelector("#app"));
+    counter++;
+} 
+
+setInterval(secsCounter, 1000)
